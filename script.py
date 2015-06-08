e26ee47e9de2cab43bd4b1dee138aee1a1716562
etijskens <engelbert.tijskens@uantwerpen.be>
2015-06-08 15:07:55 +0200

    wip on mvc separation

--- script.py
+++ script.py
@@ -1,62 +1,30 @@
 
 
 
-# ################################################################################
-# class Shebang(ScriptLine):
-# ################################################################################
-#     default = '"#!/bin/bash"'
-#     def __init__(self,text=None):
-#         self.text = Shebang.default if not text else text
+################################################################################
+class Shebang(ScriptLine):
+    default = '"#!/bin/bash"'
+    def __init__(self,text=None):
+        self.text = Shebang.default if not text else text
     
 ################################################################################
 class Comment(object):
-################################################################################
-
-    @classmethod
-    def parse(cls,text):
-        
-        
-    def __init__(self,text):
-        self.text = text
-################################################################################
-class ScriptLine(object):
-################################################################################
-    derivatives = [Comment]
-    @classmethod
-    def parse(cls,text):
-        for Derived in cls.derivatives:
-            parsed Derived.parse(text)
-        
-        return ScriptLine(text)
-    def __init__(self,text):
-        self.text = text
-
-
-################################################################################
-def parse_comment(text):
-################################################################################
-    children = [parse_comment]
-    if text.startswith('#'):
-        for parse in children:
-            parsed = parse(text)
-            if parsed:
-                return parsed
-        
-    return False
-
-################################################################################
-def parse_line(text):
-################################################################################
-    children = [parse_comment]
-    def __init__(self,text):
-        self.parse(text)        
-    def parse(self,text):
-        self.text = text
-        
-################################################################################
-class NotParsed(Exception):
     pass
 
+################################################################################
+class ScriptLine(object):
+    Derived = [Comment]
+    @classmethod
+    def parse(cls,text):
+        for D in cls.Derived:
+            parsed = D.parse(text)
+            if parsed:
+                return parsed
+        return cls(text)
+    
+    def __init__(self,text):
+        self.text = text
+        
 ################################################################################
 class Script(object):
     def __init__(self,lines=None):
