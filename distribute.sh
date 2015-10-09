87b73e311560f2c7f8fcede5fe738913399628f4
etijskens <engelbert.tijskens@uantwerpen.be>
2015-10-09 14:18:18 +0200

    improved logging

---
+++ distribute.sh
@@ -0,0 +1,10 @@
+echo Copying Launcher.app
+cp -R ~/qws/build-Launcher-Desktop_Qt_5_5_0_clang_64bit-Release/Launcher/Launcher.app ~/qws/build-Launcher-Desktop_Qt_5_5_0_clang_64bit-Release/distribute/macosx/
+
+echo Running macdeployqt
+~/QtNew/5.5/clang_64/bin/macdeployqt ~/qws/build-Launcher-Desktop_Qt_5_5_0_clang_64bit-Release/distribute/macosx/Launcher.app -dmg
+
+echo cat qt.conf
+cat ~/qws/build-Launcher-Desktop_Qt_5_5_0_clang_64bit-Release/distribute/macosx/Launcher.app/Contents/resources/qt.conf
+
+. ./rename_dmg.sh
