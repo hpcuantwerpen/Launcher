5966943a381d4eeb724c841cdaa7b51a11c0b206
etijskens-win32 <engelbert.tijskens@uantwerpen.be>
2015-10-30 15:01:06 +0100

    windows7 64bit distribution script

--- distribute/win7/distribute_win7.py
+++ distribute/win7/distribute_win7.py
@@ -33,24 +33,43 @@ def file_from_template(filename, template, variables):
         o.write(line)
     o.close()
     
+def print_title(title):
+    print '\n'+(80*'=')
+    print title+'\n'
+    
+def enter_dir(path):
+    os.chdir(path)
+    print 'Entering',path
+    
 if __name__=='__main__':
-    cwd = os.getcwd()
-    print 'cwd:',cwd
-
-    build_dir  = os.path.abspath( os.path.join('..','..','..','build-Launcher-Desktop_Qt_5_5_1_MinGW_32bit-Release') )
+    enter_dir( os.getcwd() )
+    
+    print_title('looking for build directory')
+    for build_dir in [ os.path.abspath( os.path.join('..','..','..','build-Launcher-Desktop_Qt_5_5_1_MinGW_32bit-Release') )
+                     , os.path.abspath( os.path.join('..','..','..','build-Launcher-Desktop_Qt_5_5_0_MinGW_32bit-Release') )
+                     ]:
+        if os.path.isdir(build_dir):
+            print '  FOUND    :', build_dir, '\n'
+            
+            break
+        else:
+            print '  not found:', build_dir
+    else:
+        raise RuntimeError('No build directory found')
+    
     source_dir = os.path.abspath( os.path.join('..','..') )
     build_distribute_win7_dir = os.path.join(build_dir,'distribute','win7')
     
-    os.chdir( source_dir )
-    print os.getcwd()
+    enter_dir( source_dir )
     #git_revision
+    print_title('retrieving git revision number')
     subprocess.call(['bash', 'git_revision.sh'])
     #   msys/1.0/bin must be on the path
     git_revision = open('revision.txt','r').readlines()[0][0:-1]
     _OUT_FILE_ = 'Launcher-installer-win7-'+platform.architecture()[0]+'-'+git_revision+'.exe'
 
-    
     #qmake
+    print_title('Executing qmake')
     os.chdir(build_dir)
     Qt_mingw492_32_bin = 'C:'+os.sep+'Qt'+os.sep+'5.5'+os.sep+'mingw492_32'+os.sep+'bin'
     qmake_exe = os.path.join(Qt_mingw492_32_bin,'qmake')
@@ -60,6 +79,11 @@ if __name__=='__main__':
     #make
     Qt_Tools_mingw492_32_bin = 'C:'+os.sep+'Qt'+os.sep+'\Tools'+os.sep+'mingw492_32'+os.sep+'bin'
     make_exe = os.path.join(Qt_Tools_mingw492_32_bin,'mingw32-make')
+    
+    print_title('Executing mingw32-make clean')
+    subprocess.call([make_exe,'clean'])
+    
+    print_title('Executing ming32-make')
     subprocess.call([make_exe])
 
 
@@ -70,15 +94,17 @@ if __name__=='__main__':
     
     #copy the executable
     executable_file = os.path.join(build_dir,'Launcher','release','Launcher.exe')
-    print 'copying:', executable_file
+    print_title('Copying executable:'+executable_file)
     shutil.copy(executable_file, build_distribute_win7_dir)
     assert os.path.exists(os.path.join(build_distribute_win7_dir,'Launcher.exe'))
 
     #copy the clusters directory
+    print_title('Copying clusters directory (.info files)')
     shutil.copytree(os.path.join(source_dir,'Launcher','clusters'),os.path.join(build_distribute_win7_dir,'clusters'))
     assert os.path.exists(os.path.join(build_distribute_win7_dir,'clusters'))
         
     #windeployqt
+    print_title('Deploying')
     os.chdir(build_distribute_win7_dir)
     subprocess.call(['windeployqt', '--no-translations', 'Launcher.exe'])
     # windeployqt must be on the path
@@ -87,6 +113,7 @@ if __name__=='__main__':
     shutil.copy2( os.path.join(Qt_mingw492_32_bin,'libgcc_s_dw2-1.dll'),'.')
 
     #create installer using NSIS
+    print_title('Creating installer: '+_OUT_FILE_ )
     source_distribute_win7_dir = os.path.join(source_dir,'distribute','win7')
     
     os.chdir(build_distribute_win7_dir)
@@ -108,8 +135,7 @@ if __name__=='__main__':
     subprocess.call(['makensis', 'win7.nsi'])
     # makensis must be in the path
     
+    print_title('Copying installer to Dropbox: '+_OUT_FILE_)
     shutil.copy2(_OUT_FILE_,'e:Dropbox')
     
-    print '\nCopied', _OUT_FILE_, 'to e:Dropbox (host_etijskens //vboxsrv)'
- 
-    print '\ndone'
+    print_title('Finished')
